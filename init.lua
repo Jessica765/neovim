9791e6b3ffa1d3cd747b958ba944f341a1fd052b
Jessica Harrison <harrison.taylajessica@gmail.com>
2025-03-09 19:01:30 +0200

    feat: added java and rust lsp

--- init.lua
+++ init.lua
@@ -661,7 +661,18 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {
+          -- cmd = { ... },
+          -- filetypes = { ... },
+          -- capabilities = {},
+          settings = {
+            Java = {
+              completion = {
+                callSnippet = 'Replace',
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -682,6 +693,18 @@ require('lazy').setup({
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
+        java_language_server = {
+          -- cmd = { ... },
+          -- filetypes = { ... },
+          -- capabilities = {},
+          settings = {
+            Java = {
+              completion = {
+                callSnippet = 'Replace',
+              },
             },
           },
         },
